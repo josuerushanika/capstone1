e5fe9ef855e9632af44b224f682044e1c83bad42
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-01-12 05:54:49 -0800

    Update js file

--- main.js
+++ main.js
@@ -14,3 +14,37 @@ function menuPopup() {
 }
 hamburger.addEventListener('click', menuPopup);
 popup.addEventListener('click', menuPopup);
+
+// programs object list
+
+const programs = [
+  {
+    image: 'images/intro.png',
+    heading: 'Introduction',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quia ut, eveniet possimus nam facilis quae dolores aut eum inventore nemo minus assumenda!',
+  },
+
+  {
+    image: 'images/cryptography.png',
+    heading: 'Cryptography',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quia ut, eveniet possimus nam facilis quae dolores aut eum inventore nemo minus assumenda!',
+  },
+
+  {
+    image: 'images/smartContract.png',
+    heading: 'Smart Contracts',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quia ut, eveniet possimus nam facilis quae dolores aut eum inventore nemo minus assumenda!',
+  },
+
+  {
+    image: 'images/coding.jpeg',
+    heading: 'Blockchain Development',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quia ut, eveniet possimus nam facilis quae dolores aut eum inventore nemo minus assumenda!',
+  },
+
+  {
+    image: 'images/web-3.0.webp',
+    heading: 'Web 3.0',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quia ut, eveniet possimus nam facilis quae dolores aut eum inventore nemo minus assumenda!',
+  },
+];
